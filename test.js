6aba2607cafe3b6df79088362af4398fae232c13
John Adeleke <89088298+gostlite@users.noreply.github.com>
2022-06-22 15:52:52 +0100

    Add files via upload
    m

---
+++ test.js
@@ -0,0 +1,7 @@
+var person = {
+    name: "Goke",
+    height: "6ft",
+    nationality: "Nigerian"
+}
+
+alert("My name is " +person.name + ",I am " + person.height + " tall and i am a  " + person.nationality + " by birth");
